31ce3fc7f7dead3846f480dcdd1215cea543e3b2
Leandro <leandrosantosconceicao@gmail.com>
2023-11-28 16:25:27 -0300

    Alterando localização

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'routes/routes.dart';
 
@@ -37,6 +38,8 @@ class MyApp extends StatelessWidget {
       ),
       onGenerateRoute: RouteGenerator.onGenerateRoute,
       initialRoute: RouteGenerator.home,
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+      supportedLocales: const [Locale('pt')],
       // home: const HomePage(),
     );
   }
